47a9167a0ba2b3221c71a278fe2bd7c81169fdb2
Damien Carcel <damien.carcel@akeneo.com>
2017-01-05 14:28:25 +0100

    Issue 24: Update tests for branch master

--- run_tests.sh
+++ run_tests.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
 images=("php" "fpm" "apache-php" "akeneo" "akeneo-behat" "akeneo-fpm" "akeneo-behat-fpm")
-versions=("7.1" "7.1" "php-7.1" "php-7.1" "php-7.1" "php-7.1" "php-7.1")
 
 cwd=$(pwd)
 for (( i=0; i<${#images[@]}; i++ ));
 do
     cd ${cwd}/${images[i]}
-    docker build -t carcel/${images[i]}:${versions[i]} .
+    docker build -t carcel/${images[i]} .
 done
