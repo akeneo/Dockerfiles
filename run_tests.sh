21016ebedb33b6af42f6fee56f82c40bbaf6c1e3
Damien Carcel <damien.carcel@akeneo.com>
2017-01-05 14:01:38 +0100

    Issue 24: Update tests for branch php-7.0

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 images=("php" "fpm" "apache-php" "akeneo" "akeneo-behat" "akeneo-fpm" "akeneo-behat-fpm")
-versions=("5.6" "5.6" "php-5.6" "php-5.6" "php-5.6" "php-5.6" "php-5.6")
+versions=("7.0" "7.0" "php-7.0" "php-7.0" "php-7.0" "php-7.0" "php-7.0")
 
 cwd=$(pwd)
 for (( i=0; i<${#images[@]}; i++ ));
