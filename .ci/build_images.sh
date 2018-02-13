cd28c165883dd16fbfe534d581b15b6de5f5b9ab
Damien Carcel <damien.carcel@akeneo.com>
2018-02-13 17:25:12 +0100

    Issue 283: Move 7.1 Dockerfile in dedicated folder

--- .ci/build_images.sh
+++ .ci/build_images.sh
@@ -4,15 +4,11 @@ set -euo pipefail
 
 IMAGES="php fpm apache-php"
 TAG_PREFIX="php-"
-LATEST_PHP_VERSION="7.1"
 
 PROJECT_DIR=$(dirname $(readlink -f $0))/../
 
 for IMAGE in ${IMAGES}; do
-    IMAGE_PATH=${PROJECT_DIR}/${IMAGE}
-    if [ "$PHP_VERSION" != "$LATEST_PHP_VERSION" ]; then
-        IMAGE_PATH=${IMAGE_PATH}/${PHP_VERSION}
-    fi
+    IMAGE_PATH=${PROJECT_DIR}/${IMAGE}/${PHP_VERSION}
 
     IMAGE_TAG=${PHP_VERSION}
     if [ "php" != "$IMAGE" ]; then
