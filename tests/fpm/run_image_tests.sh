1b357b61afee844551253a69b4f0dddd9aff23f7
jmleroux <jmleroux.pro@gmail.com>
2021-02-11 18:40:31 +0100

    BH-351: Github action

--- tests/fpm/run_image_tests.sh
+++ tests/fpm/run_image_tests.sh
@@ -10,11 +10,11 @@ CURRENT_DIR=$(dirname $(readlink -f $0))
 docker run -d --name akeneo-fpm-${IMAGE_TAG} -u docker -v ${CURRENT_DIR}:${CURRENT_DIR} akeneo/fpm:${IMAGE_TAG}
 
 for TEST in $(ls -1 ${CURRENT_DIR}/common/*.sh 2> /dev/null || true); do
-    docker exec akeneo-fpm-${IMAGE_TAG} bash ${TEST} || DID_FAIL=1
+    docker exec -t akeneo-fpm-${IMAGE_TAG} bash ${TEST} || DID_FAIL=1
 done
 
 for TEST in $(ls -1 ${CURRENT_DIR}/${IMAGE_TAG}/*.sh 2> /dev/null || true); do
-    docker exec akeneo-fpm-${IMAGE_TAG} bash ${TEST} || DID_FAIL=1
+    docker exec -t akeneo-fpm-${IMAGE_TAG} bash ${TEST} || DID_FAIL=1
 done
 
 docker rm -f -v akeneo-fpm-${IMAGE_TAG}
